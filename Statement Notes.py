5e6a733bf82298569869bcf5edc8c76c43c8df24
DmattyD <46036547+DmattyD@users.noreply.github.com>
2019-09-16 20:13:10 -0600

    Create Statement Notes.py

---
+++ Statement Notes.py
@@ -0,0 +1,48 @@
+Other Languages
+
+if (x)
+    if(y)
+        code-statement;
+else
+    another-code-statement;
+
+Python
+
+if x:
+    if y:
+        code-statement
+else:
+    another-code-statement
+Note how Python is so heavily driven by code indentation and whitespace. This means that code readability is a core part of the design of the Python language.
+
+Now let's start diving deeper by coding these sort of statements in Python!
+
+Time to code!
+if True:
+    print('Its True!')
+Its True!
+hungry = False
+
+if hungry:
+    print('Feed Me!')
+else:
+    print("Im Not Hungry")
+Im Not Hungry
+loc = 'zoo'
+
+if loc == 'Auto Shop':
+    print('Cars are cool!')
+elif loc == 'Bank':
+    print('Money is cool!')
+else:
+    print('I dont know much')
+I dont know much
+name = 'Sammy'
+
+if name == 'Frankie':
+    print('Hello Frankie!')
+elif name == 'Sammy':
+    print('Hello Sammy')
+else: 
+    print("What's your name?")
+Hello Sammy
